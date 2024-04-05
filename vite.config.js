6d5a05fb741a6a4468180676276cf12d743d4aa1
NishkamSikh <165985036+NishkamSikh@users.noreply.github.com>
2024-04-05 13:07:32 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,24 @@
-// import {defineConfig} from "vite";
-// import react from "@vitejs/plugin-react";
+import {defineConfig} from "vite";
+import react from "@vitejs/plugin-react";
 
 
-// export default defineConfig({
-//     plugins: [react()],
-//     server: {
-//         port:5173,
-//         open: true
-// }
-
-
-// });
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  build: { manifest: true, outDir: './dist'},
-  base:  '/',
-  root: './src',
-  plugins: [react()],
-})
+    plugins: [react()],
+    server: {
+        port:5173,
+        open: true
+}
+
+
+});
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   build: { manifest: true, outDir: './dist'},
+//   base:  '/',
+//   root: './src',
+//   plugins: [react()],
+// })
