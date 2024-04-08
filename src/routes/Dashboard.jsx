e3dd4deea5c45b882587f3c61f931078559351f4
Karamjit Singh <karamjitmann@gmail.com>
2024-04-08 12:48:36 -0400

    Dashbaord Data Update

--- src/routes/Dashboard.jsx
+++ src/routes/Dashboard.jsx
@@ -11,6 +11,7 @@ const Dashboard = () => {
     })
     return (
         <section>
+            <h2>Dashaboard</h2>
 
             {/* <div className="">
                 <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
