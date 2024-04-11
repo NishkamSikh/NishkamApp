4b1b3b0b818ea84d615c4f887f93517bff5413a6
Karamjit <karamjitmann@gmail.com>
2024-04-11 01:31:07 -0400

    Simmi mam changes 5

--- src/routes/StudentProfileAdd.jsx
+++ src/routes/StudentProfileAdd.jsx
@@ -546,6 +546,7 @@ const StudentProfile = () => {
                 </button>
                 <button
                   type="submit"
+                  disabled={!formData.studentcode || !formData.stuyear || !formData.firstname || !formData.lastname}
                   style={{ opacity: formData.studentcode && formData.stuyear && formData.firstname && formData.lastname && formData.gender && formData.status ? 1 : 0.5 }}
                   className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
