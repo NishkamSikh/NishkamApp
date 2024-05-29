a04de5b09d17f223502eaf0c9b09ac504b71c47c
Karamjit <karamjitmann@gmail.com>
2024-05-29 19:56:50 -0400

    Donor LIst 8pm

--- src/routes/DonorData/DonorProfileList.jsx
+++ src/routes/DonorData/DonorProfileList.jsx
@@ -111,7 +111,8 @@ const DonorProfileList = () => {
         const fetchData = async () => {
             try {
                 // Make API request using fetch
-                const response = await fetch('https://nishkamapi.onrender.com/api/v1/donorlist');
+               const response = await fetch('https://nishkamapi.onrender.com/api/v1/donorlist');
+                //const response = await fetch('http://localhost:3000/api/v1/donorlist');
                 setFetchData(false);
                 // Check if the response status is ok (200-299)
                 if (!response.ok) {
