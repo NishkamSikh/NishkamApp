54ac7867801bfd2bb8cbb388f93b2065ec06d5af
Karamjit <karamjitmann@gmail.com>
2024-06-01 00:34:08 -0400

    Beneficiary List 10

--- src/routes/Reports/StudentSummaryList.jsx
+++ src/routes/Reports/StudentSummaryList.jsx
@@ -48,7 +48,7 @@ const StudentSummaryList = () => {
         {
             id:'55',
             name: 'Photo',
-            selector: row => <img src={row.DocumentURL} width={50} height={50}></img>,
+            selector: row => <img src={row.DocumentURL} width={40} height={40}></img>,
             sortable: false,
             compact: true,
             width: "4rem",
