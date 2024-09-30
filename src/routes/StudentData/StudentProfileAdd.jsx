c46db9f0a3dc5cb104cab0188f29481259650c5f
Karamjit <karamjitmann@gmail.com>
2024-09-29 22:25:16 -0400

    Sep 29-1

--- src/routes/StudentData/StudentProfileAdd.jsx
+++ src/routes/StudentData/StudentProfileAdd.jsx
@@ -190,9 +190,7 @@ const StudentProfile = () => {
 
                         >
                           <option >Select Year</option>
-                          <option value="2023-2024">2023-2024</option>
-                          <option value="2022-2023">2022-2023</option>
-                          <option value="2021-2022">2021-2022</option>
+                          <option value="2024-2025" selected>2024-2025</option>
                         </select>
                       </div>
                     </div>
