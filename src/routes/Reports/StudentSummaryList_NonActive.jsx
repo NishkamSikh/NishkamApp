f0429eec060b43e6920ba52fc0db94ffd95c2507
Karamjit <karamjitmann@gmail.com>
2025-03-05 21:53:27 -0500

    20250305-2

--- src/routes/Reports/StudentSummaryList_NonActive.jsx
+++ src/routes/Reports/StudentSummaryList_NonActive.jsx
@@ -296,8 +296,8 @@ const StudentSummaryList = () => {
             try {
                 // Make API request using fetch
 //                const url = new URL('https://sikligarapi.azurewebsites.net/api/v1/fetchAllStudentSummary?AcademicYear='+AcademicYear);
- //                  const url = new URL('https://sikligarapi.azurewebsites.net/api/v1/fetchAllStudentSummary_Active?AcademicYear='+AcademicYear);
-                  const url = new URL('http://localhost:3000/api/v1/fetchAllStudentSummary_NonActive?AcademicYear='+AcademicYear);
+                   const url = new URL('https://sikligarapi.azurewebsites.net/api/v1/fetchAllStudentSummary_Active?AcademicYear='+AcademicYear);
+ //                 const url = new URL('http://localhost:3000/api/v1/fetchAllStudentSummary_NonActive?AcademicYear='+AcademicYear);
 
 
                 const response = await fetch(url);
